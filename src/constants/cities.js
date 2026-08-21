8a63a8412cf3a015bd0d3dcced97872417059d52
santy62kumar <s.santoshkumar6205@gmail.com>
2026-08-21 11:20:39 +0530

    added the city

--- src/constants/cities.js
+++ src/constants/cities.js
@@ -1,5 +1,5 @@
 export const CITIES = [
   "Mumbai", "Pune", "Bangalore", "Delhi", "Kochi",
   "Chennai", "Rohtak", "Mohali", "Mysore",
-  "Hyderabad", "Tirupati", "Lucknow", "Nanded", "Namakkal",
+  "Hyderabad", "Tirupati", "Lucknow", "Nanded", "Namakkal", "Deharadun",
 ];
